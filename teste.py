414197b9829691d7fd81e8dde200d21eb69791bc
Matheus Eidt <mreidt@localhost.localdomain>
2018-10-16 20:48:17 -0300

    Adicionando busca na api do google

--- teste.py
+++ teste.py
@@ -1,10 +1,52 @@
 import pandas as pd
+import requests
 
 
 data = pd.read_csv('data_points_20180101.txt', delimiter=':|  Bearing:', names=['tipo', 'valores', 'bearing'], header=None)
 
+GOOGLE_MAPS_API_KEY = 'AIzaSyCeWfCykKZLpMrg83oDcRoto_Aw4mHsyZM'
 
+def separa_endereco(response, dataFrame):
+    # R. Monsenhor Veras, 405 - Santana, Porto Alegre - RS, 90610-010, Brazil
+    # TODO: verificar casos em que nao recebe o endereco completo
+    for r in response:
+        print(r)
+        print('\n')
+    # print(response)
+    rua, numBairro, cidadeUf, cep, pais = response.split(',')
+    numero, bairro = numBairro.split('-')
+    cidade, uf = cidadeUf.split('-')
 
+    dataFrame['rua']=rua
+    dataFrame['numero']=numero
+    dataFrame['bairro']=bairro
+    dataFrame['cidade']=cidade
+    dataFrame['estado']=uf
+    dataFrame['cep']=cep
+    dataFrame['pais']=pais
+
+    print('--------------------\n')
+    # return rua.strip(), numero.strip(), bairro.strip(), cidade.strip(), uf.strip(), cep.strip(), pais.strip()
+
+def busca_endereco(dataFrame):
+    latitude=dataFrame['latitude']
+    longitude=dataFrame['longitude']
+    base = "https://maps.googleapis.com/maps/api/geocode/json?"
+    params = "latlng={lat},{lon}&key={key}".format(
+        lat=latitude,
+        lon=longitude,
+        key=GOOGLE_MAPS_API_KEY
+    )
+    url = "{base}{params}".format(base=base, params=params)
+    print(url)
+    response = requests.get(url)
+    status = response.json()['status']
+    if status == 'OK':
+        print('Tudo certo')
+        separa_endereco(response=response.json()['results'], dataFrame=dataFrame)
+    else:
+        print('Problemas')
+        return status
 #testeA['tipo'] = latitudes['tipo']
 #testeA['graus'], testeA['decimal'] = latitudes['valores'].str.split('   ', 1).str
 
@@ -33,4 +75,9 @@ df['distancia'] = distances['valores']
 df['bearing'] = distances['bearing']
 # print(df.to_string().encode('utf-8').decode('latin-1'))
 
+
+
+df.apply(lambda x: busca_endereco(x), axis=1)
+print('\n')
 print(df)
+# df['rua'],df['numero'],df['bairro'],df['cidade'],df['estado'],df['cep'],df['pais'] = busca_endereco(lat, lon)
