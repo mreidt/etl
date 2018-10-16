eb4a2fa784b0b60ca665c0e89c047028826b847e
matheus.eidt <matheus.reidt@gmail.com>
2018-10-16 16:32:14 -0300

    Separando valor de latitude e longitude em duas colunas. Para adicionar na outra df precisa filtrar.

--- teste.py
+++ teste.py
@@ -13,5 +13,14 @@ print(distances.to_string().encode('utf-8').decode('latin-1'))
 #testeA['tipo'] = latitudes['tipo']
 #testeA['graus'], testeA['decimal'] = latitudes['valores'].str.split('   ', 1).str
 
-data['valores_split'] = data['valores'].str.split('   ')
+data = data.join(data['valores'].str.split('   ', 1, expand=True).rename(columns={0:'graus', 1:'decimal'}))
+
+#data['valores_split'] = data['valores'].str.split('   ')
 print(data.to_string().encode('utf-8').decode('latin-1'))
+
+colunas = ['latitude', 'longitude', 'rua', 'numero', 'bairro', 'cidade', 'cep', 'estado', 'pais']
+df = pd.DataFrame(columns=colunas)
+df['latitude'] = data['decimal']
+df['longitude'] = 
+
+print(df)
