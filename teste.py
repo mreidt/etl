01eee32f57bf6a1cd607491a4ee1153e490b352a
Matheus Eidt <mreidt@localhost.localdomain>
2018-10-17 19:33:33 -0300

    adicionando novo script criacao da tabela de forma certa

--- teste.py
+++ teste.py
@@ -1,9 +1,12 @@
 import pandas as pd
 import requests
+from sqlalchemy import create_engine
+from pandas.io import sql
 
-data = pd.read_csv('data_points_20180102.txt', delimiter=':|  Bearing:', names=['tipo', 'valores', 'bearing'], header=None)
+data = pd.read_csv('data_points_20180101.txt', delimiter=':|  Bearing:', names=['tipo', 'valores', 'bearing'], header=None)
 
 GOOGLE_MAPS_API_KEY = 'AIzaSyCeWfCykKZLpMrg83oDcRoto_Aw4mHsyZM'
+columns = ['LATITUDE', 'LONGITUDE', 'RUA', 'NUMERO', 'BAIRRO', 'CIDADE', 'CEP', 'ESTADO', 'PAIS', 'LATITUDEGRAUS', 'LONGITUDEGRAUS', 'DISTANCIA', 'BEARING']
 
 def address_resolver(json, dataFrame):
     final = {}
@@ -13,17 +16,17 @@ def address_resolver(json, dataFrame):
             data[category + '_long'] = item['long_name']
             data[category + '_short'] = item['short_name']
 
-    dataFrame['rua'] = data.get("route_long", None)
-    dataFrame['estado'] = data.get("administrative_area_level_1_short", None)
-    dataFrame['cidade'] = data.get("administrative_area_level_2_long", None)
-    dataFrame['pais'] = data.get("country_long", None)
-    dataFrame['cep'] = data.get("postal_code_long", None)
-    dataFrame['bairro'] = data.get("sublocality_long",None)
-    dataFrame['numero'] = data.get('street_number_long', None)
+    dataFrame['RUA'] = data.get("route_long", None)
+    dataFrame['ESTADO'] = data.get("administrative_area_level_1_short", None)
+    dataFrame['CIDADE'] = data.get("administrative_area_level_2_long", None)
+    dataFrame['PAIS'] = data.get("country_long", None)
+    dataFrame['CEP'] = data.get("postal_code_long", None)
+    dataFrame['BAIRRO'] = data.get("sublocality_long",None)
+    dataFrame['NUMERO'] = data.get('street_number_long', None)
 
 def busca_endereco(dataFrame):
-    latitude=dataFrame['latitude']
-    longitude=dataFrame['longitude']
+    latitude=dataFrame['LATITUDE']
+    longitude=dataFrame['LONGITUDE']
 
     print('\n')
     print('{lat},{lon}'.format(lat=latitude, lon=longitude))
@@ -57,14 +60,14 @@ print(latitudes.to_string().encode('utf-8').decode('latin-1'))
 print(longitudes.to_string().encode('utf-8').decode('latin-1'))
 print(distances.to_string().encode('utf-8').decode('latin-1'))
 
-colunas = ['latitude', 'longitude', 'rua', 'numero', 'bairro', 'cidade', 'cep', 'estado', 'pais', 'latitudeGraus', 'longitudeGraus', 'distancia', 'bearing']
-df = pd.DataFrame(columns=colunas)
-df['latitude'] = latitudes['decimal']
-df['longitude'] = longitudes['decimal']
-df['latitudeGraus'] = latitudes['graus']
-df['longitudeGraus'] = longitudes['graus']
-df['distancia'] = distances['valores']
-df['bearing'] = distances['bearing']
+# colunas = ['latitude', 'longitude', 'rua', 'numero', 'bairro', 'cidade', 'cep', 'estado', 'pais', 'latitudeGraus', 'longitudeGraus', 'distancia', 'bearing']
+df = pd.DataFrame(columns=columns)
+df['LATITUDE'] = latitudes['decimal']
+df['LONGITUDE'] = longitudes['decimal']
+df['LATITUDEGRAUS'] = latitudes['graus']
+df['LONGITUDEGRAUS'] = longitudes['graus']
+df['DISTANCIA'] = distances['valores']
+df['BEARING'] = distances['bearing']
 # print(df.to_string().encode('utf-8').decode('latin-1'))
 
 
@@ -72,4 +75,13 @@ df['bearing'] = distances['bearing']
 df.apply(lambda x: busca_endereco(x), axis=1)
 print('\n')
 print(df)
+
+engine = create_engine("mysql+pymysql://{user}:{pw}@localhost/{db}"
+                       .format(user="root",
+                               pw="root",
+                               db="ETLDB"))
 # df['rua'],df['numero'],df['bairro'],df['cidade'],df['estado'],df['cep'],df['pais'] = busca_endereco(lat, lon)
+df.to_sql(con=engine, name='ENDERECOS', if_exists='append', index=False)
+
+
+# (latitude, longitude, rua, numero, bairro, cidade, cep, estado, pais, `latitudeGraus`, `longitudeGraus`, distancia, bearing)
